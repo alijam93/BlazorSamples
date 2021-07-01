5bfc15556dbbe63c29c244e4d0b32d59ea9c1c91
ajam93 <52130390+ajam93@users.noreply.github.com>
2021-07-01 23:21:40 +0430

    add outside event for select

---
+++ wwwroot/app.js
@@ -0,0 +1,13 @@
+﻿function SelectToggle(triggerId, selectId) {
+    var x = document.getElementById(selectId);
+    if (x.style.display === "block")
+        x.style.display = "none";
+    else
+        x.style.display = "block";
+
+    document.addEventListener("click", (e) => {
+        if (!document.getElementById(triggerId).contains(e.target)) {
+            x.style.display = "none";
+        }
+    });
+}
