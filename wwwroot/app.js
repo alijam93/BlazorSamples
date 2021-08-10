ec03e8e404fbfcdf59faef17bb4e2f22e5e47470
ajam93 <52130390+ajam93@users.noreply.github.com>
2021-08-10 19:37:58 +0430

    add scroll indicator

--- wwwroot/app.js
+++ wwwroot/app.js
@@ -11,3 +11,12 @@
         }
     });
 }
+
+function verticalIndicator() {
+    window.addEventListener("scroll", function () {
+        let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+            height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+            scrolled = (winScroll / height) * 100;
+        document.getElementById("progressBar").style.width = scrolled + "%";
+    });
+}
