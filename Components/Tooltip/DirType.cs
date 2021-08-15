1663184d462822e3eef89998005dc4c307293bd5
ajam93 <52130390+ajam93@users.noreply.github.com>
2021-08-15 17:52:19 +0430

    add tooltip

---
+++ Components/Tooltip/DirType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSamples.Components.Tooltip
+{
+    public static class DirType
+    {
+        public const string Top = "tooltip-top";
+        public const string Right = "tooltip-right";
+        public const string Left = "tooltip-left";
+        public const string Bottom = "tooltip-bottom";
+    }
+}
